2de09be76428e753c46fc2eda714a9b6e673db2c
Federico Gosman <federico@equipogsm.com>
2018-05-31 18:33:14 -0300

    Refactor account receivable. Fix last payment due bug

--- credisur/__main__.py
+++ credisur/__main__.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 from datetime import datetime
+import pkg_resources
 
 import credisur.datastructures as datastructures
 import credisur.dateintelligence as dateintelligence
@@ -10,6 +11,7 @@ import credisur.exceladapter as exceladapter
 import credisur.excelbuilder as excelbuilder
 import credisur.tableextraction as tableextraction
 
+from credisur.models import AccountReceivable
 
 # TODO: Copiar listado de facturas en solapa.
 # TODO: Controlar consistencia código (por ejemplo, D-E está mal.
@@ -28,6 +30,7 @@ def valid_date(s):
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Script para procesar planillas de Credisur')
+    parser.add_argument("--version", "-v", help="Muestra la versión.", action="store_true")
     parser.add_argument("--inputs", "-i", help="Permite definir la carpeta de inputs", default="inputs")
     parser.add_argument("--outputs", "-o", help="Permite definir la carpeta de outputs", default="outputs")
     parser.add_argument(
@@ -54,6 +57,10 @@ def main(args=None):
 
     parser = get_parser()
     params = parser.parse_args(args)
+
+    if params.version:
+        print(pkg_resources.require("credisuretl")[0].version)
+        return
 
     inputs_path = "%s/%s/" % (cwd, params.inputs)
     outputs_path = "%s/%s/" % (cwd, params.outputs)
@@ -83,19 +90,16 @@ def main(args=None):
 
         config_list.append(("E", "Fecha de Compra", 'date_of_purchase'))
         config_list.append(("F", "Fecha de Vencimiento", 'due_date'))
-        config_list.append(("G", "Valor de compra", 'total_purchase_value'))
 
-        config_list.append(("H", "Orden de Compra", 'order'))
-        config_list.append(("I", "Última Cobranza", 'last_collection'))
+        config_list.append(("G", "Orden de Compra", 'order'))
+        config_list.append(("H", "Última Cobranza", 'last_collection'))
 
-        config_list.append(("J", "Cuotas", 'plan'))
-        config_list.append(("K", "Saldo Total", 'debt_balance'))
-        config_list.append(("L", "Cuotas a pagar", 'current_payment'))
-        config_list.append(("M", "Valor de cuota", 'payment'))
-        config_list.append(("N", "Saldo vencido", 'past_due_debt'))  # revisar
-        config_list.append(("O", "Deuda impaga a la fecha", 'overdue_balance'))
+        config_list.append(("I", "Cuotas", 'plan'))
 
-        config_list.append(("P", "Monto total a cobrar", 'amount_to_collect'))
+        config_list.append(("J", "Cuotas a pagar", 'current_payment'))
+        config_list.append(("K", "Valor de cuota", 'payment'))
+
+        config_list.append(("L", "Monto total a cobrar", 'amount_to_collect'))
         return config_list
 
     def get_old_excel_filename(filename):
@@ -234,10 +238,6 @@ def main(args=None):
     accounts_to_collect_unpacker = tableextraction.TableUnpacker(accounts_to_collect_sheet)
 
     for row_unpacker in accounts_to_collect_unpacker.read_rows(2):
-        account_to_collect = {}
-
-        overdue_balance = 0
-        past_due_debt = ""
 
         document_date = row_unpacker.get_value_at(1)
         due_date = row_unpacker.get_value_at(2)
@@ -245,177 +245,44 @@ def main(args=None):
         customer = row_unpacker.get_value_at(4)
         raw_code = row_unpacker.get_value_at(9)
 
+        if "Cobranza" in document: continue
+        if not raw_code: continue
+
+        line_amount = float(row_unpacker.get_value_at(8))
+        line_balance = row_unpacker.get_value_at(8)
+
         customer_data = customers[customer]
 
-        city = customer_data['city']
-        phone = customer_data['phone']
-        address = customer_data['address']
+        account_receivable = AccountReceivable(document_date, due_date, document,
+                                               customer, raw_code, customer_data, line_amount,
+                                               line_balance, calendar_ops)
 
-        if "Cobranza" in document:
-            continue
+        if not account_receivable.is_historic_and_due_for(last_date_of_month): continue
 
-        if not raw_code:
-            continue
 
-        version = get_version_from_code(raw_code)
+        if not account_receivable.validate_payment_plan(errors): continue
 
-        if version == HISTORICO and due_date > last_date_of_month:
-            continue
 
-        list_of_codes = raw_code.split("-")
+        account_receivable.configure_previous_collections(collections)
+        account_receivable.compute_last_collection_date()
 
-        collection_account, collection_person, sales_order, *_ = list_of_codes
 
-        collections_for_order = []
-        last_collection_date = "Sin cobranza previa"
-        current_payment_number = 1
+        account_receivable.compute_due_payment(bills, first_day_of_current_month, last_date_of_month)
 
-        if sales_order and sales_order in collections:
-            collections_for_order = collections[sales_order]
+        account_receivable.validate_low_advance_payment(100, errors)
+        account_receivable.validate_advance_payment(errors)
 
-        if sales_order and len(collections_for_order) > 0:
-            last_collection_date = sorted(collections_for_order, key=lambda x: x['date'], reverse=True)[0][
-                'date'].strftime(
-                "%d/%m/%Y")
+        if not account_receivable.validate_total_sale_amount_and_plan_value(errors): continue
 
-        if version == HISTORICO:
-            current_payment_number, plan = list_of_codes[3].split(" de ")
-            current_payment_number = int(current_payment_number)
-            plan = int(plan)
-            payment_amount = float(row_unpacker.get_value_at(8))
-            total_purchase_value = payment_amount * int(plan)
+        if not account_receivable.has_due_payments(customers_without_payments_due): continue
 
-            debt_balance = ""
-            advance_payment = ""
-
-            current_payment_description = str(current_payment_number)
-
-        else:
-            if len(list_of_codes) < 5:
-                error = "Cuenta a cobrar sin valor de cuota. Documento: %s - Descripción: %s" % (document, raw_code)
-                errors.append(error)
-                continue
-
-            plan = int(list_of_codes[3])
-            payment_amount = float(list_of_codes[4])
-            debt_balance = row_unpacker.get_value_at(8)
-
-            total_purchase_value = bills[document]
-            paid_amount = total_purchase_value - debt_balance
-            advance_payment = total_purchase_value - (plan * payment_amount)
-
-            if 100 >= advance_payment > 0:
-                total_plan_amount = str(int(plan * payment_amount))
-                error = "El valor del anticipo es muy bajo ($ %s). Comprobar que no se trate de un error de carga. " \
-                       "%s. Cliente: %s. Orden de Compra: %s. Valor total de Factura: %s. " \
-                       "Valor total según plan: %s" % (
-                    advance_payment, document, customer, sales_order, total_purchase_value, total_plan_amount )
-                errors.append(error)
-
-            past_payments = 0
-
-            if total_purchase_value < (plan * payment_amount):
-                error = "El monto de venta es menor al plan de pagos. " \
-                        "Documento: %s - Valor: %s. Plan: %s - Cuota: %s. Diferencia: %s" % (
-                            document, total_purchase_value, plan, payment_amount, advance_payment)
-                errors.append(error)
-                continue
-
-            if paid_amount > advance_payment:
-                past_payments = int((paid_amount - advance_payment) // payment_amount)
-
-            if advance_payment < 0:
-                error = "El monto de venta es menor al plan de pagos. " \
-                        "Documento: %s - Valor: %s. Plan: %s - Cuota: %s. Diferencia: %s" % (
-                            document, total_purchase_value, plan, payment_amount, advance_payment)
-                errors.append(error)
-
-            due_dates = calendar_ops.list_of_due_date(due_date, plan)
-            due_payments = next((plan - i for i, v in enumerate(reversed(due_dates)) if v < first_day_of_current_month),
-                                0)
-            due_payments_with_current = next(
-                (plan - i for i, v in enumerate(reversed(due_dates)) if v <= last_date_of_month), 0)
-            current_payment_number_by_date = current_payment_number = next(
-                (plan - i for i, v in enumerate(reversed(due_dates)) if v <= last_date_of_month), 0)
-            past_due_debt = advance_payment + (due_payments * payment_amount)
-            overdue_balance = past_due_debt - paid_amount
-
-            overdue_advance_payment_amount = (advance_payment - paid_amount) if advance_payment - paid_amount > 0 else 0
-
-            missing_payments = due_payments_with_current - past_payments
-
-            if not missing_payments > 0:
-                customer_without_payment_descrption = "%s - %s (orden: %s) - %s. Monto vencido: %s. Monto cobrado: %s. Primer vencimiento: %s" % (
-                    city, customer, sales_order, address or 'Sin dirección',
-                    past_due_debt, paid_amount, due_date.strftime("%d/%m/%Y")
-                )
-                customers_without_payments_due.append(customer_without_payment_descrption)
-                continue
-
-            missing_payment_numbers = list(current_payment_number_by_date - x for x in range(missing_payments))
-            partial_debt_in_payment = overdue_balance % payment_amount
-
-            partial_debt_in_payment_without_advance = partial_debt_in_payment - overdue_advance_payment_amount
-
-            if partial_debt_in_payment_without_advance > 0:
-                first_payment_description = "+$%s de cuota %s" % (
-                    int(partial_debt_in_payment_without_advance),
-                    missing_payment_numbers[-1])
-                missing_payment_numbers[-1] = first_payment_description
-
-            if overdue_advance_payment_amount > 0:
-                advance_payment_description = "+$%s de anticipo" % (
-                    int(overdue_advance_payment_amount)
-                    )
-                missing_payment_numbers.append(advance_payment_description)
-
-            current_payment_description = ", ".join(str(i) for i in missing_payment_numbers)
-
-        if plan == current_payment_number:
-            customer_details = "%s - %s - %s" % (city, customer, address or 'Sin dirección')
-            customers_in_last_payment.append(customer_details)
-
-        document_date_str = document_date.strftime("%d/%m/%Y")
-        due_date_str = due_date.strftime("%d/%m/%Y")
-
-        amount_to_collect = float(payment_amount) + float(overdue_balance)
-
-        account_to_collect['version'] = version
-
-        account_to_collect['document'] = document
-        account_to_collect['date_of_purchase'] = document_date_str
-
-        account_to_collect['due_date_datetime'] = due_date
-        account_to_collect['due_date'] = due_date_str
-
-        account_to_collect['customer'] = customer
-
-        account_to_collect['city'] = city
-        account_to_collect['address'] = address
-        account_to_collect['phone'] = phone
-
-        account_to_collect['account'] = collection_account
-        account_to_collect['person'] = collection_person
-
-        account_to_collect['order'] = sales_order
-
-        account_to_collect['last_collection'] = last_collection_date
-        account_to_collect['total_purchase_value'] = total_purchase_value
-        account_to_collect['plan'] = plan
-        account_to_collect['advance_payment'] = advance_payment
-        account_to_collect['debt_balance'] = debt_balance
-
-        account_to_collect['current_payment'] = current_payment_description
-        account_to_collect['payment'] = payment_amount
-        account_to_collect['past_due_debt'] = past_due_debt
-        account_to_collect['overdue_balance'] = overdue_balance
-
-        account_to_collect['amount_to_collect'] = amount_to_collect
+        account_receivable.add_to_list_if_in_last_payment(customers_in_last_payment)
+        account_to_collect = account_receivable.to_dict()
 
         if not account_to_collect['city'] or not account_to_collect['customer']:
             print("FALTA CIUDAD O CLIENTE:", account_to_collect['city'], account_to_collect['customer'])
 
-        accounts_to_collect[collection_account].append(account_to_collect)
+        accounts_to_collect[account_to_collect['account']].append(account_to_collect)
 
     for error in errors:
         print("ADVERTENCIA:", error)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='credisuretl',
-      version='0.1.6',
+      version='0.1.7',
       description='ETL for Felsim',
       url='http://github.com/fedegos/credisuretl',
       author='Federico Gosman',
