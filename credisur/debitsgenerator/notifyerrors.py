a9aa32868c34a19c7143ffe6b1c9ddc4160332e6
Federico Gosman <fedegos@gmail.com>
2019-02-13 17:41:58 -0300

    Add newline on errors stream write

--- credisur/debitsgenerator/notifyerrors.py
+++ credisur/debitsgenerator/notifyerrors.py
@@ -3,4 +3,4 @@
 def notify_errors(notification, stream):
     for error in notification.iterate_errors():
         print("ERROR:", error)
-        stream.write(error)
+        stream.write(error + "\n")
