3c78685320bfa6ad6c74380b2f6766f2106de600
Federico Gosman <fedegos@gmail.com>
2019-02-13 15:41:41 -0300

    Add debit lines validation

--- credisur/debitsgenerator/parsers/parsedebitslines.py
+++ credisur/debitsgenerator/parsers/parsedebitslines.py
@@ -9,19 +9,70 @@ def parse_debits_lines(page_data):
         line_mapping = map_line(line_num, page_data)
         debit_lines.append(DebitLine(line_mapping))
 
+    for line in debit_lines:
+        print(line.cbu_bloque_1, line.cbu_bloque_2, line.monto)
+
     return debit_lines
 
 
 def map_line(row, page_data):
     return {
+        'sheet_name': page_data.title,
+        'row': row,
         'tipo_novedad': page_data.cell(row=row, column=1).value,
         'cbu_bloque_1':  page_data.cell(row=row, column=2).value,
         'cbu_bloque_2': page_data.cell(row=row, column=3).value,
         'id_cliente': page_data.cell(row=row, column=4).value,
-        'monto': page_data.cell(row=row, column=5).value,
+        'monto': format_amount(page_data.cell(row=row, column=5).value),
     }
+
+
+def format_amount(amount):
+    return str(round(amount * 100)).zfill(8)
+
 
 class DebitLine:
 
     def __init__(self, line_mapping):
-        pass
+        self._sheet_name = line_mapping['sheet_name']
+        self._row = line_mapping['row']
+        self._tipo_novedad = line_mapping['tipo_novedad']
+        self._cbu_bloque_1 = line_mapping['cbu_bloque_1']
+        self._cbu_bloque_2 = line_mapping['cbu_bloque_2']
+        self._id_cliente = line_mapping['id_cliente']
+        self._monto = line_mapping['monto']
+
+
+    @property
+    def sheet_name(self):
+        return self._sheet_name
+
+
+    @property
+    def row(self):
+        return self._row
+
+
+    @property
+    def tipo_novedad(self):
+        return self._tipo_novedad
+
+
+    @property
+    def cbu_bloque_1(self):
+        return self._cbu_bloque_1
+
+
+    @property
+    def cbu_bloque_2(self):
+        return self._cbu_bloque_2
+
+
+    @property
+    def id_cliente(self):
+        return self._id_cliente
+
+
+    @property
+    def monto(self):
+        return self._monto
--- credisur/debitsgenerator/validators/validatelines.py
+++ credisur/debitsgenerator/validators/validatelines.py
@@ -1,7 +1,41 @@
 from .errorsiterator import ErrorsIterator
 
-
 def validate_lines(debits_data):
     errors = ErrorsIterator()
 
+    map_func = validate_line_func(errors)
+
+    map(map_func, debits_data)
+
     return errors
+
+def validate_line_func(errors):
+
+    def validate_line(line):
+        """
+
+        :type line: credisur.debitsgenerator.parsers.parsedebitslines.DebitLine
+        """
+
+        row = line.row
+        sheet_name = line.sheet_name
+
+        def add_error(error):
+            errors.add_error(error + " en fila %s de solapa %s" % (row, sheet_name))
+
+        if not line.tipo_novedad:
+            add_error("Falta el 'Tipo Novedad'")
+
+        if not line.cbu_bloque_1:
+            add_error("Falta el 'CBU Bloque 1'")
+
+        if not line.cbu_bloque_2:
+            add_error("Falta el 'CBU Bloque 2'")
+
+        if not line.id_cliente:
+            add_error("Falta el 'ID Cliente'")
+
+        if not line.monto:
+            add_error("Falta el 'Monto'")
+
+    return validate_line
