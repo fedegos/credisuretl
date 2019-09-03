20b86122ffaf644b81b801a212cf53f6d70cd776
Federico Gosman <fedegos@gmail.com>
2019-09-03 15:06:36 -0300

    Fixed #22 column ordering from Xubio 201909

--- credisur/extractors/customer_row_extractor.py
+++ credisur/extractors/customer_row_extractor.py
@@ -4,8 +4,8 @@ def customer_row_extractor(results, unpacker):
     name = unpacker.get_value_at(1)
 
     result['address'] = unpacker.get_value_at(8)
-    result['city'] = unpacker.get_value_at(33) # Cambio de Xubio 2019 04 (de 29 a 33)
+    result['city'] = unpacker.get_value_at(32) # Cambio de Xubio 2019 09 (de 33 a 32)
     result['phone'] = unpacker.get_value_at(12)
-    result['cbu'] = unpacker.get_value_at(5)
+    result['cbu'] = unpacker.get_value_at(5) # La columna CBU ya estaba configurada como 5
 
     results[name] = result
