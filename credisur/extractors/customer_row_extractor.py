f46f06434ace140891c8b38f93873bf071e4fb1d
Federico Gosman <fedegos@gmail.com>
2019-09-06 14:02:36 -0300

    Fixed CBU position on Xubio export file

--- credisur/extractors/customer_row_extractor.py
+++ credisur/extractors/customer_row_extractor.py
@@ -6,6 +6,6 @@ def customer_row_extractor(results, unpacker):
     result['address'] = unpacker.get_value_at(8)
     result['city'] = unpacker.get_value_at(32) # Cambio de Xubio 2019 09 (de 33 a 32)
     result['phone'] = unpacker.get_value_at(12)
-    result['cbu'] = unpacker.get_value_at(5) # La columna CBU ya estaba configurada como 5
+    result['cbu'] = unpacker.get_value_at(6) # La columna CBU ya estaba configurada como 5
 
     results[name] = result
