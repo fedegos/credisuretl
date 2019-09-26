eb3b6ad3842210af915e0c483fb916b9ca3d5bb0
Federico Gosman <fedegos@gmail.com>
2019-09-26 16:19:35 -0300

    Fixed #24

--- credisur/extractors/customer_row_extractor.py
+++ credisur/extractors/customer_row_extractor.py
@@ -4,8 +4,8 @@ def customer_row_extractor(results, unpacker):
     name = unpacker.get_value_at(1)
 
     result['address'] = unpacker.get_value_at(8)
-    result['city'] = unpacker.get_value_at(32) # Cambio de Xubio 2019 09 (de 33 a 32)
+    result['city'] = unpacker.get_value_at(33) # Cambio de Xubio 2019 09 (de 33 a 32) - 2do cambio en 2019 09 (de 32 a 33)
     result['phone'] = unpacker.get_value_at(12)
-    result['cbu'] = unpacker.get_value_at(6) # La columna CBU ya estaba configurada como 5
+    result['cbu'] = unpacker.get_value_at(6) # Cambio de Xubio 2019 09 (de 5 a 6)
 
     results[name] = result
